5ad18d76575044cf914927bf72f3fb2f3c5fcaa7
Noah Robison-Cox <noahrc@gmail.com>
2012-11-15 04:05:40 -0500

    changed structure

--- README.md
+++ README.md
@@ -2,3 +2,38 @@ carobare
 ========
 
 A barebones carousel jQuery plugin using data-attributes
+
+## Examples
+
+View examples here [nrcdesign.com/carobare](http://nrcdesign.com/carobare)
+
+## Usage
+
+Load jquery and then jquery.carobare.js
+
+### Data attributes option
+
+Specify options on a parent element around an image to make it a carousel, like this:
+	
+	<figure id="carousel-1" class="figure carousel" data-carobare-width="320" data-carobare-height="255" data-carobare-url="images/" data-carobare-slides="example1.jpg example2.jpg example3.jpg" data-carobare-interval="2000" >
+				<img src="images/example1.jpg" alt="Sharing insights" />
+			</figure>
+
+Then initialize the plugin like this:
+
+	$('#carousel-1').carobare();
+
+### Javascript option
+
+Specify options in the carobare funciton like this:
+	
+	$('#carousel-2').carobare(
+	{
+		'interval'	: 3000,
+		'width'		: 320,
+		'height'	: 255,
+		'slides'	: 'example1.jpg example2.jpg example3.jpg',
+		'urlPrefix'	: 'images/'
+	});
+
+View the source for more options.
--- jquery.carobare.js
+++ jquery.carobare.js
@@ -10,10 +10,9 @@
 
 ;(function($) {
 	
-	$.fn.carobare = function(options) { 
-
-		// Default settings
-		var settings = $.extend({
+	// Default settings
+	var pluginName = 'carobare'
+		, defaults = {
 			'width' 	: 'auto',	// Width of the carousel container
 			'height' 	: 'auto',	// Height of the carousel container
 			'interval' 	: 4000,		// Time between image changes
@@ -21,78 +20,109 @@
 			'urlPrefix'	: '',		// optional URL prefix to go before the names of images to load
 			'clipping'	: false,	// If true, images will be cropped
 			'border'	: true		// If true, adds a border around the images
-		}, options);
+		};
 
-		return this.each(function (i) {
-			var $c = $(this)
-				, carouselWidth = $c.attr('data-carobare-width') || settings.width
-				, carouselHeight = $c.attr('data-carobare-height') || settings.height
-				, carouselInterval = $c.attr('data-carobare-interval') || settings.interval
-				, carouselSlides = $c.attr('data-carobare-slides') || settings.slides
-				, urlPrefix = $c.attr('data-carobare-url') || settings.urlPrefix
-				, carouselClipping = $c.attr('data-carobare-clipping') || settings.clipping
-				, carouselBorder = $c.attr('data-carobare-border') || settings.border;
-			var CAROUSEL = {};
+	// Constructor
+	function Carobare (element, options) {
+		this.element = element;
+		this.settings = $.extend({}, defaults, options);
+		this.init();
+	}
 
-			CAROUSEL.showNextSlide = function () {
-				var $next = $c.find('img.active').removeClass('active').fadeOut(1000).next('img');
-				$next[0] ? $next : $next = $($c.find('img')[0]);
-				$next.addClass('active').fadeIn(1000);
-			}
-			
-			if(typeof carouselSlides !== 'undefined' && carouselSlides !== null ) {
-				$.each($.isArray(carouselSlides) ? carouselSlides : carouselSlides.split(' '),
-					function(i, val) {
-						var url = urlPrefix + this
-							, $img = $c.find('img[src$="' + url + '"]')
-							, $newImg;
-							
-						// If this image isn't on the page already, add it
-						if(!$img[0]) {
-							$newImg = $('<img alt="" src="' + url + '" />').appendTo($c).css(
-							{
-								'position'	: 'absolute', 
-								'left'		: 0,
-								'top'		: 0
-							});
+	Carobare.prototype.init = function () {
+		
+		var $c = $(this.element)
+			, settings = this.settings
+			, carouselWidth = $c.attr('data-carobare-width') || settings.width
+			, carouselHeight = $c.attr('data-carobare-height') || settings.height
+			, carouselInterval = $c.attr('data-carobare-interval') || settings.interval
+			, carouselSlides = $c.attr('data-carobare-slides') || settings.slides
+			, urlPrefix = $c.attr('data-carobare-url') || settings.urlPrefix
+			, carouselClipping = $c.attr('data-carobare-clipping') || settings.clipping
+			, carouselBorder = $c.attr('data-carobare-border') || settings.border;
 
-						};
+		if(typeof carouselSlides !== 'undefined' && carouselSlides !== null ) {
+			$.each($.isArray(carouselSlides) ? carouselSlides : carouselSlides.split(' '),
+				function(i, val) {
+					var url = urlPrefix + this
+						, $img = $c.find('img[src$="' + url + '"]')
+						, $newImg;
 
-						// Hide new images
-						($newImg && i>0) && $newImg.hide();
-						$img.css({
+					// If this image isn't on the page already, add it
+					if(!$img[0]) {
+						$newImg = $('<img alt="" src="' + url + '" />').appendTo($c).css(
+						{
 							'position'	: 'absolute', 
-							'left'		: 0, 
+							'left'		: 0,
 							'top'		: 0
 						});
-					}
-				);
-			}
 
-			$c.css(
-			{
-				'position'	: 'relative',
-				'overflow'	: carouselClipping ? 'hidden' : 'visible',
-				'width'		: carouselWidth,
-				'height'	: carouselHeight
+					};
+
+					// Hide new images
+					($newImg && i>0) && $newImg.hide();
+					$img.css({
+						'position'	: 'absolute', 
+						'left'		: 0, 
+						'top'		: 0
+					});
+				}
+			);
+		}
+
+		$c.css(
+		{
+			'position'	: 'relative',
+			'overflow'	: carouselClipping ? 'hidden' : 'visible',
+			'width'		: carouselWidth,
+			'height'	: carouselHeight
+		});
+
+		if(carouselBorder) {
+			$c.find('img').css({
+				'padding'	: '10px',
+				'border'	: '1px solid #CCC'
 			});
+			$c.css({
+				'width'		: '+=22',
+				'height'	: '+=22'
+			});
+		};
+		this.startSlideInterval(carouselInterval);
+	}
 
-			if(carouselBorder) {
-				$c.find('img').css({
-					'padding'	: '10px',
-					'border'	: '1px solid #CCC'
-				});
-				$c.css({
-					'width'		: '+=22',
-					'height'	: '+=22'
-				});
-			};
-			
-			$c.showNextSlideInterval = function() {
-				CAROUSEL.showNextSlide();
-			}
+	// Maintains the local scope when calling methods with bind
+	Carobare.prototype.bind = function( Method ){
 
-			$c.carouseltimer = setInterval($c.showNextSlideInterval, carouselInterval);
+        var _this = this;
+
+        return(
+             function(){
+             return( Method.apply( _this, arguments ) );
+             }
+        );
+    }     
+
+	Carobare.prototype.startSlideInterval = function (interval) {
+		var closure = this.bind(this.showNextSlide);
+		var timer = setInterval(closure, interval);
+	}
+
+	Carobare.prototype.showNextSlide = function () {
+		var $c = $(this.element);
+		var $next = $c.find('img.active').removeClass('active').fadeOut(1000).next('img');
+		$next[0] ? $next : $next = $($c.find('img')[0]);
+		$next.addClass('active').fadeIn(1000);
+	}
+	
+	$.fn[pluginName] = function(options) { 
+
+		return this.each(function (i) {
+			if (!$.data(this, 'plugin_' + pluginName)) {
+                $.data(this, 'plugin_' + pluginName,
+                new Carobare( this, options ));
+            }
 		});
 	}
+
 })(jQuery);
